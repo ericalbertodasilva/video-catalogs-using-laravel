1af072277451fc1a91ee3d29fe8c6db632724b1d
ericalbertodasilva <ercker@gmail.com>
2020-11-28 10:26:04 -0300

    test: http genre

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -2,6 +2,8 @@
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
 chown -R www-data:www-data .
+cp .env.exemple .env
+cp .env.testing.exemple .env.testing
 composer install
 php artisan key:generate
 php artisan migrate
--- app/Http/Controllers/Api/GenreController.php
+++ app/Http/Controllers/Api/GenreController.php
@@ -21,7 +21,9 @@ class GenreController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, $this-> rules);
-        Genre::create($request -> all());
+        $genre=Genre::create($request -> all());
+        $genre->refresh();
+        return $genre;
     }
 
 
