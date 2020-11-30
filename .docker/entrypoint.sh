e516a19dac9edbf7433ab8954c005caeba8a86bf
Eric A Silva <ercker@gmail.com>
2020-11-30 14:41:31 +0000

    fix: file permission

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -2,8 +2,6 @@
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
 chown -R www-data:www-data .
-cp .env.exemple .env
-cp .env.testing.exemple .env.testing
 composer install
 php artisan key:generate
 php artisan migrate
